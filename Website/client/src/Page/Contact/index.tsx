44dac7cf27f22eeed6d21202d651e7cdc4240cbc
waldo van dijk <waldo_vandijk@hotmail.com>
2021-11-19 00:06:53 +0100

    Form test

--- Website/client/src/Page/Contact/index.tsx
+++ Website/client/src/Page/Contact/index.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { BreakPoints } from '../../Style';
 import { ContactData } from '../../Data';
 import { useState } from 'react';
+import Axios, { AxiosResponse } from 'axios';
 
 const ContactWrapper = styled('div')`
   grid-column: 2/3;
@@ -37,6 +38,21 @@ const FormInput = styled('input')`
     border: 0.3rem solid #677c8b;
   }
 `;
+
+const TextField = styled('textarea')`
+  box-sizing: border-box;
+  width: 100%;
+  border: 0.3rem solid #677c8b;
+  font-size: 1.6rem;
+  padding: 1rem;
+  margin-bottom: 2rem;
+  resize: none;
+  height: 20rem;
+  &:focus,
+  :active {
+    border: 0.3rem solid #677c8b;
+  }
+`;
 const ContactDetails = styled('div')`
   font-size: 2rem;
 `;
@@ -51,15 +67,50 @@ const DetailsContent = styled('div')`
   padding-bottom: 0.5rem;
 `;
 
+const InputButton = styled('input')`
+  background-color: orange;
+  border: none;
+  padding: 1rem 3rem;
+  color: white;
+  font-size: 1.6rem;
+  cursor: pointer;
+  &:hover {
+    background-color: #ffb833;
+  }
+`;
+
 const FormWrapper = styled('form')``;
 const Contact = () => {
   const { title, subTitle, details } = ContactData;
   const [subject, setSubject] = useState<string>('');
   const [email, setEmail] = useState<string>('');
+  const [textField, setTextField] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
+
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const response: AxiosResponse<string> = await Axios({
+      method: 'POST',
+      baseURL: 'https://voyagersamsterdam.com',
+      url: '/auth/login',
+      data: {
+        subject: subject,
+        email: email,
+        content: textField,
+      },
+      validateStatus: null,
+    });
+    if (response.status !== 200) {
+      setMessage('Message Send!');
+    } else {
+      setMessage('Message failed to send, Use own email!');
+    }
+  };
+
   return (
     <ContactWrapper>
       <ContactTitle>{title}</ContactTitle>
-      <FormWrapper>
+      <FormWrapper onSubmit={onSubmit}>
         <FormTitle>{subTitle}</FormTitle>
         <FormInput
           value={subject}
@@ -72,9 +123,17 @@ const Contact = () => {
           value={email}
           type='email'
           onChange={(e) => setEmail(e.target.value)}
-          placeholder='Email *'
+          placeholder='Your email *'
           required
         />
+        <TextField
+          value={textField}
+          onChange={(e) => setTextField(e.target.value)}
+          placeholder='Message *'
+          required
+        />
+        <InputButton type='submit' value='Send' />
+        {message && <div>{message}</div>}
       </FormWrapper>
       <ContactDetails>
         <DetailsTitle>{details.title}</DetailsTitle>
